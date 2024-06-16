c0c94e9789d351e974152b41c23e9130feefd416
sujayverma124 <sujayverma124@gmail.com>
2024-06-16 16:00:20 +0530

    Updated the image files

---
+++ www/index.php
@@ -0,0 +1,7 @@
+<h1 style="text-align:center">Hi, Sujay it's working!</h1>
+<?php 
+      // echo "<pre>";
+      // print_r(get_loaded_extensions());
+      // echo "<pre/>";
+ ?>
+<?php phpinfo(); ?>
